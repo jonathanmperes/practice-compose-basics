e56ae115339d29a64a5e5491c57ca249d45ba661
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-01-11 10:44:54 -0300

    task manager completed

--- app/src/main/java/com/app/practicecomposebasics/MainActivity.kt
+++ app/src/main/java/com/app/practicecomposebasics/MainActivity.kt
@@ -9,12 +9,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.resolveDefaults
-import androidx.compose.ui.text.style.TextAlign.Companion.Justify
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,51 +38,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ShowContentWithImage() {
-    val image = painterResource(id = R.drawable.bg_compose_background)
-    Column {
-        Image(
-            painter = image,
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth(),
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val image = painterResource(id = R.drawable.ic_task_completed)
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = stringResource(id = R.string.task_completed),
+            fontWeight = Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
         )
-        ShowTitle()
-        ShowDescription()
-        ShowText()
+        Text(
+            text = stringResource(id = R.string.nice_work),
+            fontSize = 16.sp
+        )
     }
-}
 
-@Composable
-fun ShowTitle() {
-    Text(
-        text = stringResource(R.string.jetpack_title),
-        fontSize = 24.sp,
-        textAlign = Justify,
-        modifier = Modifier
-            .padding(all = 16.dp)
-    )
 }
-
-@Composable
-fun ShowDescription() {
-    Text(
-        text = stringResource(R.string.jetpack_description),
-        textAlign = Justify,
-        modifier = Modifier
-            .padding(start = 16.dp, end = 16.dp)
-    )
-}
-
-@Composable
-fun ShowText() {
-    Text(
-        text = stringResource(R.string.jetpack_text),
-        textAlign = Justify,
-        modifier = Modifier
-            .padding(all = 16.dp)
-    )
-}
-
 
 @Preview(showBackground = true)
 @Composable
